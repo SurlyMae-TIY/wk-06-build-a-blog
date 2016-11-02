9cda7fe8e82a155ff528bf526509d23be035a708
SurlyMae <surlymae@gmail.com>
2016-11-02 16:48:18 -0600

    feat(core): Add sidebar component

---
+++ src/app/components/Sidebar.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+export default class Sidebar extends React.Component {
+  render () {
+    return(
+      <div>
+        <h2>Sidebar</h2>
+        <h3>sidebar things</h3>
+      </div>
+    );
+  }
+}
