84867ff4cd56cb8fa8cb036ba097b1c0b5273b7f
SurlyMae <surlymae@gmail.com>
2016-11-05 14:42:56 -0600

    feat(core): Add sidebar component with buttons

--- src/app/components/Body/Sidebar.js
+++ src/app/components/Body/Sidebar.js
@@ -3,11 +3,26 @@
 import React from 'react';
 
 export default class Sidebar extends React.Component {
+  handleChange(e) {
+    this.props.changeData();
+  }
+
   render () {
     return(
-      <div>
-        <h2>Sidebar</h2>
-        <h3>sidebar things</h3>
+      <div className="clickers">
+        <h3>{this.props.data}</h3>
+        <button onChange={this.handleChange.bind(this)}>September</button>
+        <button onChange={this.handleChange.bind(this)}>October</button>
+        <button onChange={this.handleChange.bind(this)}>November</button>
+        <button onChange={this.handleChange.bind(this)}>"toast"</button>
+        <button onChange={this.handleChange.bind(this)}>"pour-over"</button>
+        <button onChange={this.handleChange.bind(this)}>"distillery"</button>
+        <button onChange={this.handleChange.bind(this)}>"chicharrones"</button>
+        <button onChange={this.handleChange.bind(this)}>"vape"</button>
+        <button onChange={this.handleChange.bind(this)}>"PBR"</button>
+        <button onChange={this.handleChange.bind(this)}>"meditation"</button>
+        <button onChange={this.handleChange.bind(this)}>"artisan"</button>
+        <button onChange={this.handleChange.bind(this)}>"cred"</button>
       </div>
     );
   }
---
+++ src/app/components/Main/Sidebar.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+export default class Sidebar extends React.Component {
+  render () {
+    return(
+      <div>
+        <h3>Search here!</h3>
+        <input />
+      </div>
+    );
+  }
+}
